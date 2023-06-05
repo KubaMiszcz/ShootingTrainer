9eafe6c5ded3f5098b1b1c5132270acf881fd009
KubaMiszcz <zielonyeufor@gmail.com>
2023-06-05 21:43:39 +0200

    play sonuds works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ShootingTimer';
+
+  play(){
+    let a= new Audio();
+
+  }
+  playAudio(){
+    let audio = new Audio();
+    // audio.src = "../../../assets/audio/alarm.wav";
+    audio.src = "../assets/mywav.wav";
+    audio.load();
+    audio.play();
+  }
+  
 }
