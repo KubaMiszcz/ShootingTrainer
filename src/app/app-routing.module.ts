85e343d5dc6ba5fbc595df377694ab63bba436b2
KubaMiszcz <zielonyeufor@gmail.com>
2023-06-21 21:34:54 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayProcedureTabComponent } from './components/play-procedure-tab/play-procedure-tab.component';
+import { EditProcedureTabComponent } from './components/edit-procedure-tab/edit-procedure-tab.component';
+import { TimerTabComponent } from './components/timer-tab/timer-tab.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'play-procedure-tab', component: PlayProcedureTabComponent },
+  { path: 'edit-procedure-tab', component: EditProcedureTabComponent },
+  { path: 'timer-tab', component: TimerTabComponent },
+  { path: '', redirectTo: '/play-procedure-tab', pathMatch: 'full' },
+  { path: '**', component: PlayProcedureTabComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { AppService } from './services/app.service';
 import { Component } from '@angular/core';
-import { AppSettingsService } from './services/app-settings.service';
-import { IStage } from './models/stage';
-import { AudioPlayerService } from './services/audio-player.service';
 
 @Component({
   selector: 'app-root',
@@ -11,61 +7,4 @@ import { AudioPlayerService } from './services/audio-player.service';
 })
 export class AppComponent {
   title = 'ShootingTimer';
-  defaultProcedure: IStage[];
-  currentActionLabel = '';
-  currentActionIdx = 0;
-  currentStageIdx = 0;
-
-  constructor(
-    private appSettings: AppSettingsService,
-    private appService: AppService,
-    private audioPlayerService: AudioPlayerService
-  ) {
-    this.defaultProcedure = appSettings?.defaultProcedure;
-
-    this.audioPlayerService.playbackEndedBS.subscribe((data) =>
-      this.playAction(data)
-    );
-  }
-
-  private playAction(data: string) {
-    // console.log(data);
-    let actions = this.defaultProcedure[this.currentStageIdx]?.actions ?? [];
-    let action = actions[this.currentActionIdx];
-
-    if (!!action) {
-      setTimeout(() => {
-        this.currentActionLabel = action.name;
-        console.log(this.currentActionLabel);
-        this.audioPlayerService.playAction(
-          this.appSettings.audioFilesPath + action.audioFileName
-        );
-      }, Number(action?.delay_sec) * 1000);
-    }
-
-    this.currentActionIdx = this.currentActionIdx + 1;
-  }
-
-  playStage() {
-    this.currentActionIdx = 0;
-    this.audioPlayerService.playbackEndedBS.next('start');
-  }
-
-  playProcedure() {
-    this.currentStageIdx = 0;
-
-    this.defaultProcedure.forEach(stage => {
-      console.log(stage);
-      
-      this.playStage();
-      this.currentStageIdx = this.currentStageIdx + 1;
-    });
-  }
-
-  // recordAudio() {
-  //   navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-  //     const mediaRecorder = new MediaRecorder(stream);
-  //     mediaRecorder.start();
-  //   });
-  // }
 }
